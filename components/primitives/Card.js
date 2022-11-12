40e6581c5504ad0ba1fdfc9568cfd3c357c84f8d
justin_k_m <justinm0623@gmail.com>
2022-11-11 19:59:35 -0500

    Card.js - Added Under Construction default state

--- components/primitives/Card.js
+++ components/primitives/Card.js
@@ -1,9 +1,21 @@
 import styles from '@/scss/card.module.scss'
 
-export default function Card() {
-  return (
-    <div className={styles.container}>
+import UnderConstruction from '@/prefabs/cards/UnderConstruction'
 
+export default function Card(props) {
+  const propStyle = {
+    width: props.width,
+    height: props.height,
+    ...props.style
+  }
+
+  const children = props.children || (<UnderConstruction/>)
+
+  return (
+    <div className={`${styles.container} ${props.className}`}
+      style={propStyle}
+    >
+      {children}
     </div>
   )
 }
