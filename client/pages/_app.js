41fe39c79ae73c71aa43703970268eaa7bc35c6c
justin_k_m <justinm0623@gmail.com>
2022-11-24 01:26:14 -0500

    Client Server integration

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,22 @@
 import 'styles/globals.scss'
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import Layout from 'components/Layout'
 
 export default function MyApp({ Component, pageProps }) {
 
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <Layout>
-      <Component
-        {...pageProps}
-      />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component
+          {...pageProps}
+        />
+      </Layout>
+    </ApolloProvider>
   )
 }
