b08e471a0263aad53ea9cfc8bea1be86256ef650
justin_k_m <justinm0623@gmail.com>
2022-11-24 01:26:37 -0500

    Successful querying, validating username

--- client/components/prefabs/Modals.js
+++ client/components/prefabs/Modals.js
@@ -4,16 +4,34 @@ import styles from '@/scss/modals.module.scss'
 
 import { reactiveState } from 'models/cache'
 
-import { useReactiveVar } from '@apollo/client'
+import { useState } from 'react'
+
+import { gql, useLazyQuery, useQuery } from '@apollo/client'
 
 export function ModalLogin() {
 
-  const r_state = useReactiveVar(reactiveState)
-
+  const [username, modalLogin_setUsername] = useState('')
+  const [password, modalLogin_setPassword] = useState('')
+  const [rememberMe, modalLogin_setRememberMe] = useState(false)
+  
   const description = 'Accessing further features requires a login.\nPlease enter your credentials below!'
   
+  const VALIDATE_USERNAME = gql`
+    query ValidateUsername($username: String!) {
+      usernameExists(username: $username)
+    }
+  `
+
+  const [validateUsername, {loading, error,data}] = useLazyQuery(VALIDATE_USERNAME, { 
+    variables : { username }
+  })
+
+  function modalLogin_handleCheckbox() {
+    modalLogin_setRememberMe(!rememberMe)
+  }
+
   function modalLogin_formSubmit() {
-    console.log('Working')
+    validateUsername(username).then((resp) => console.log(resp.data))
   }
 
   function modalLogin_forgotPassword() {
@@ -31,11 +49,11 @@ export function ModalLogin() {
       
       <form className={styles.login__form}>
         <label className={styles.login__label}>Username or Email Address</label>
-        <input className={styles.login__input} type={'text'}></input>
+        <input className={styles.login__input} type={'text'} onChange={e => {modalLogin_setUsername(e.currentTarget.value)}}></input>
         <label className={styles.login__label}>Password</label>
-        <input className={styles.login__input} type={'text'}></input>
+        <input className={styles.login__input} type={'text'} onChange={e => {modalLogin_setPassword(e.currentTarget.value)}}></input>
         <div className={styles.login__checkbox_container}>
-          <input className={styles.login__checkbox} type={'checkbox'}></input>
+          <input className={styles.login__checkbox} type={'checkbox'} onChange={modalLogin_handleCheckbox}></input>
           <label className={`${styles.login__label} ${styles.login__checkbox_label}`}>Remember Me</label>
         </div>
         <input className={styles.login__form_submit} type={'button'} value={'Login'} onClick={modalLogin_formSubmit}></input>
