41b46f3798c642e4f1d745b286d8e939f8ae1e57
justin_k_m <justinm0623@gmail.com>
2022-11-24 01:25:46 -0500

    Mock Backend

--- server/server.js
+++ server/server.js
@@ -3,21 +3,26 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 
 const typeDefs = `
   type User {
-    username: String
-    password: String
+    id: String!
+    username: String!
+    password: String!
   }
 
   type Query {
-    users: [User]
+    users: [User],
+    usernames: [String],
+    usernameExists(username: String!): Boolean!
   }
 `
 
 const users = [
   {
+    id: '000',
     username: 'readmorebooks2',
     password: 'abc123'
   },
   {
+    id: '001',
     username: 'TheLegend27',
     password: 'undefeated'
   }
@@ -26,6 +31,8 @@ const users = [
 const resolvers = {
   Query: {
     users: () => users,
+    usernames: () => users.map((user) => user.username),
+    usernameExists: (parent, args) => users.filter((user) => user.username == args.username).length > 0  
   }
 }
 
