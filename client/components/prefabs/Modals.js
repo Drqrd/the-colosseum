e107ba878b86bf47f8114f833b5f89b3f5ee47ef
justin_k_m <justinm0623@gmail.com>
2022-11-25 16:30:48 -0500

    Incorrect login notification

--- client/components/prefabs/Modals.js
+++ client/components/prefabs/Modals.js
@@ -6,24 +6,24 @@ import { reactiveState } from 'models/cache'
 
 import { useState } from 'react'
 
-import { gql, useLazyQuery, useQuery } from '@apollo/client'
+import { gql, useLazyQuery } from '@apollo/client'
 
 export function ModalLogin() {
 
   const [username, modalLogin_setUsername] = useState('')
   const [password, modalLogin_setPassword] = useState('')
   const [rememberMe, modalLogin_setRememberMe] = useState(false)
+  const [correctLogin, modalLogin_correctLogin] = useState(null)
   
   const description = 'Accessing further features requires a login.\nPlease enter your credentials below!'
   
-  const VALIDATE_USERNAME = gql`
-    query ValidateUsername($username: String!) {
-      usernameExists(username: $username)
+  const TRY_LOGIN = gql`
+    query TryLogin($username: String!, $password: String!) {
+      tryLogin(username: $username, password: $password)
     }
   `
-
-  const [validateUsername, {loading, error,data}] = useLazyQuery(VALIDATE_USERNAME, { 
-    variables : { username }
+  const [tryLogin, {login_loading, login_error, login_data}] = useLazyQuery(TRY_LOGIN, { 
+    variables : { username, password }
   })
 
   function modalLogin_handleCheckbox() {
@@ -31,14 +31,49 @@ export function ModalLogin() {
   }
 
   function modalLogin_formSubmit() {
-    validateUsername(username).then((resp) => console.log(resp.data))
+    tryLogin(username, password).then((resp) => {
+      modalLogin_correctLogin(resp.data.tryLogin)
+    })
   }
 
   function modalLogin_forgotPassword() {
     modalToModalTransition('FORGOT_PASSWORD')
   }
 
-  return (
+  function modalLogin_incorrectUsernameOrPassword() {
+
+  }
+
+  const modal = correctLogin != null && !correctLogin  ? 
+    <Modal
+        className={styles.login__container}
+    >
+      <h2 className={styles.login__title}>Login</h2>
+      <div className={styles.login__description_container}>
+        <div className={styles.login__description}>{description}</div>
+      </div>
+      
+      <div className={styles.login__wrong_login}>
+        <div className={styles.login__wrong_login__title}>Wrong Credentials</div>
+        <div className={styles.login__wrong_login__description}>Incorrect Username or Password</div>
+      </div>
+
+      <form className={styles.login__form}>
+        <label className={styles.login__label}>Username or Email Address</label>
+        <input className={styles.login__input} type={'text'} onChange={e => {modalLogin_setUsername(e.currentTarget.value)}}></input>
+        <label className={styles.login__label}>Password</label>
+        <input className={styles.login__input} type={'text'} onChange={e => {modalLogin_setPassword(e.currentTarget.value)}}></input>
+        <div className={styles.login__checkbox_container}>
+          <input className={styles.login__checkbox} type={'checkbox'} onChange={modalLogin_handleCheckbox}></input>
+          <label className={`${styles.login__label} ${styles.login__checkbox_label}`}>Remember Me</label>
+        </div>
+        <input className={styles.login__form_submit} type={'button'} value={'Login'} onClick={modalLogin_formSubmit}></input>
+      </form>
+
+      <button className={`${styles.login__forgot_password} ${styles.login__label}`}
+        onClick={modalLogin_forgotPassword}
+      >Forgot Password?</button>
+    </Modal> :
     <Modal
       className={styles.login__container}
     >
@@ -62,15 +97,17 @@ export function ModalLogin() {
       <button className={`${styles.login__forgot_password} ${styles.login__label}`}
         onClick={modalLogin_forgotPassword}
       >Forgot Password?</button>
-
     </Modal>
+
+  return (
+    modal
   )
 }
 
 export function ModalForgotPassword() {
   return (
     <Modal>
-
+      
     </Modal>
   )
 }
