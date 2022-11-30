2e5f5d272dcaf25720e8bf3f174f477021e9ae22
justin_k_m <justinm0623@gmail.com>
2022-11-29 21:04:21 -0500

    _app.js - Client side authorization work

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,31 @@
 import 'styles/globals.scss'
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
 import Layout from 'components/Layout'
 
 export default function MyApp({ Component, pageProps }) {
 
-  const client = new ApolloClient({
+  const httpLink = createHttpLink({
     uri: "http://localhost:4000/",
+    credentials: 'include'
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token')
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : 'Bearer null',
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    // uri: "http://localhost:4000/",
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 
