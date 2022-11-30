c6b57bd748e117651157459b4dedf094611af4a9
justin_k_m <justinm0623@gmail.com>
2022-11-29 21:04:33 -0500

    data.js - dedicated data file

---
+++ server/data.js
@@ -0,0 +1,20 @@
+export const userData = [
+  {
+    username: 'readmorebooks2',
+    email: 'asdfasdf',
+    password: 'abc123',
+    token: 'asdfasdfasd',
+  },
+  {
+    username: 'TheLegend27',
+    email: 'asdfasdf',
+    password: 'undefeated',
+    token: 'asdfasdf',
+  },
+  {
+    username: 'sysadmin',
+    email: 'asdfasdf',
+    password: 'admin',
+    token: 'asdfasasdfa',
+  }
+]
