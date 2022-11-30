ed085365c7bbafd312439258ab2971ea76475ec6
justin_k_m <justinm0623@gmail.com>
2022-11-29 21:04:46 -0500

    index.js - dedicated resolver file

---
+++ server/graphql/resolvers/index.js
@@ -0,0 +1,12 @@
+import userResolvers from "./users.js"
+
+const resolvers = {
+  Query: {
+    ...userResolvers.Query
+  },
+  Mutation: {
+    ...userResolvers.Mutation
+  }
+}
+
+export default resolvers
