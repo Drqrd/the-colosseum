e8e36acd3ea259c3ea4e07f22ca6ab35e81dd300
justin_k_m <justinm0623@gmail.com>
2022-11-29 21:05:20 -0500

    typeDefs.js - dedicated typeDefs folder

---
+++ server/graphql/typeDefs.js
@@ -0,0 +1,31 @@
+const typeDefs =`
+type User {
+  username: String!
+  email: String!
+  password: String!
+  token: String!
+}
+
+input LoginInput {
+  username: String!
+  password: String!
+}
+
+input RegisterInput {
+  username: String!
+  password: String!
+  email: String!
+}
+
+type Query {
+  users: [User]
+  getUserById(id: ID!): User,
+  login(username: String!, password: String!): Boolean!
+}
+
+type Mutation {
+  register(registerInput: RegisterInput): User
+}
+`
+
+export default typeDefs
