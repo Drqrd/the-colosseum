ed65a4cb60bd22e531b2fb3ac50ccf8b6c4847e6
justin_k_m <justinm0623@gmail.com>
2022-12-15 14:23:06 -0500

    _app.js - fixed invalid hook error

---
+++ pages/_app.js
@@ -0,0 +1,53 @@
+import 'styles/globals.scss'
+
+import Head from 'next/head'
+
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, useReactiveVar } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+import { r_token } from 'models/cache'
+
+import Layout from 'components/Layout'
+
+export default function MyApp({ Component, pageProps }) {
+
+  function getValidToken() {
+    return r_token()
+  }
+
+  const httpLink = createHttpLink({
+    uri: "http://localhost:4000",
+  })
+ 
+
+  const authLink = setContext(async (_, { headers }) => {
+    const token = await getValidToken()
+    
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : 'Bearer null',
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    // uri: "http://localhost:4000",
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Layout>
+        <Head>
+          <title>The Colosseum</title>
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        </Head>
+        <Component
+          {...pageProps}
+        />
+      </Layout>
+    </ApolloProvider>
+  )
+}
