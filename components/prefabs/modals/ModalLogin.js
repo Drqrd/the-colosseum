8df9266b9c47f9cc9b131493e8b59e7c6e97f182
justin_k_m <justinm0623@gmail.com>
2022-12-17 16:19:53 -0500

    ModalLogin.js - Login mutation, returns token that matches server

--- components/prefabs/modals/ModalLogin.js
+++ components/prefabs/modals/ModalLogin.js
@@ -3,22 +3,22 @@ import styles from '@/scss/modals/modalLogin.module.scss'
 
 import { modalToModalTransition } from '@/prefabs/Modals'
 
-import { useLazyQuery, gql } from "@apollo/client"
+import { useLazyQuery, useMutation, gql } from "@apollo/client"
 import { r_user, r_token } from 'models/cache'
 import { useState } from 'react'
 
 
 export default function ModalLogin() {
 
-  const [username, setUsername] = useState('')
+  const [usernameOrEmail, setusernameOrEmail] = useState('')
   const [password, setPassword] = useState('')
   const [rememberMe, setRememberMe] = useState(false)
-  const [incorrectLogin, setIncorrectLogin] = useState(false)
+  const [loginState, setLoginState] = useState(null)
 
   const description = 'Accessing further features requires a login.\nPlease enter your credentials below!'
   
   const TRY_LOGIN = gql`
-    query Login($loginInput: LoginInput!) {
+    mutation Login($loginInput: LoginInput!) {
       login(loginInput: $loginInput)
     }
   `
@@ -29,16 +29,17 @@ export default function ModalLogin() {
         username
         password
         email
+        token
       }
     }
   `
 
   const [getUsers] = useLazyQuery(USERS)
 
-  const [tryLogin] = useLazyQuery(TRY_LOGIN, { 
+  const [tryLogin] = useMutation(TRY_LOGIN, { 
     variables : { 
       loginInput: {
-        username,
+        usernameOrEmail,
         password
       }
     },
@@ -57,7 +58,7 @@ export default function ModalLogin() {
   }
 
   function handleInput() {
-    tryLogin({username, password}).then((resp) => {
+    tryLogin({usernameOrEmail, password}).then((resp) => {
       if (resp.data) {
         console.log(resp)
       
@@ -67,17 +68,24 @@ export default function ModalLogin() {
         })
         
         if (resp.data.login !== 'error') {
-          console.log(token)
-          setIncorrectLogin(false)
+          setLoginState(true)
           r_token(resp.data.login)
           setTimeout(() => {
             modalToModalTransition('')
           }, 1250)
         }
         else {
-          setIncorrectLogin(true)
+          setLoginState(false)
         }
       }
+    })
+
+    getUsers().then((resp) => {
+      console.log(resp.data.users[0])
+
+      console.log(resp.data.users[0].token)
+      console.log(r_token())
+      console.log(r_token() === resp.data.users[0].token)
     })
   }
 
@@ -94,7 +102,8 @@ export default function ModalLogin() {
         onClick={register}
       >Need an Account?</button>
 
-      <ErrorMessage show={incorrectLogin}/>
+      <ErrorMessage show={loginState}/>
+      <SuccessMessage show={loginState}/>
 
       <form className={styles.login__form}
         onSubmit={ e => {
@@ -103,7 +112,7 @@ export default function ModalLogin() {
         }}
       >
         <label className={styles.login__label}>Username or Email Address</label>
-        <input className={styles.login__input} type={'text'} onChange={e => {setUsername(e.target.value)}}></input>
+        <input className={styles.login__input} type={'text'} onChange={e => {setusernameOrEmail(e.target.value)}}></input>
         <label className={styles.login__label}>Password</label>
         <input className={styles.login__input} type={'password'} onChange={e => {setPassword(e.target.value)}}></input>
         <div className={styles.login__checkbox_container}>
@@ -123,10 +132,20 @@ export default function ModalLogin() {
 }
 
 function ErrorMessage(props) {
-  return ( props.show ? 
+  return ( props.show !== null && !props.show ? 
     <div className={styles.login__wrong_login}>
       <div className={styles.login__wrong_login__title}>Wrong Credentials</div>
       <div className={styles.login__wrong_login__description}>Incorrect Input</div>
+    </div> : 
+    ''
+  )
+}
+
+function SuccessMessage(props) {
+  return ( props.show !== null && props.show ?
+    <div className={styles.login__right_login}>
+      <div className={styles.login__right_login__title}>Success!</div>
+      <div className={styles.login__right_login__description}>Logging in...</div>
     </div> : 
     ''
   )
