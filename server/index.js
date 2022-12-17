b4565da609283e623cfee7c32c4df8fcc3dc0ae8
justin_k_m <justinm0623@gmail.com>
2022-12-17 17:29:02 -0500

    index.js - removed comments

--- server/index.js
+++ server/index.js
@@ -5,11 +5,6 @@ import typeDefs from './graphql/typeDefs.js'
 import resolvers from './graphql/resolvers/index.js'
 
 import { userData } from './data.js'
-import jws from 'jsonwebtoken'
-
-// import mongoose from 'mongoose'
-
-// const mongoDB = 'mongodb+srv://Justinm0623:dpYNCFvRbuis81Ki@colosseum-cluster.iye0zem.mongodb.net/test'
 
 const getUserByToken = (token) => userData.filter((user) => user.token === token)[0]
 
@@ -29,17 +24,6 @@ const context = {
   },
   listen: { port: 4000 },
 }
-
-/*
-mongoose.connect(mongoDB, { useNewUrlParser: true})
-  .then(() => {
-    console.log('MongoDB Connected')
-    return server.listen({port: 4000})
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`)
-  })
-*/
 
 const { url } = await startStandaloneServer(server, context)
 
