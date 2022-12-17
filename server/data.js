7977d8c8ac4e39ba853496e4d0b6cb09e348d7d0
justin_k_m <justinm0623@gmail.com>
2022-12-17 17:28:52 -0500

    Update data.js

--- server/data.js
+++ server/data.js
@@ -1,6 +1,6 @@
 export const userData = [
   {
-    username: 'TestTest',
+    username: 'Test1',
     email: 'example@domain.com',
     // password: Test1234!
     password: '$2b$10$dzCOAG49HDF/et/QEKG/Eu8301K29X69rSH7ghYd9iINI3bjL69eC',
