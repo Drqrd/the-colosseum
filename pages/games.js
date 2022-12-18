a822960e45c439e91ec9c3e0a6b31f754815d550
justin_k_m <justinm0623@gmail.com>
2022-12-18 15:08:32 -0500

    Update games.js

--- pages/games.js
+++ pages/games.js
@@ -1,5 +1,37 @@
+import styles from '@/scss/games.module.scss'
+
+import Pagination from '@/primitives/Pagination'
+
 export default function Games() {
-  return (
-    <div>Games</div>
+  const items = [
+    {
+      item: 'Chess',
+      values: {
+        'value1': 20,
+        'value2': 1,
+      },
+      tags: ['tag1', 'tag2', 'tag3'],
+    },
+    {
+      item: 'Checkers',
+      values: {
+        'value3': 1,
+        'value1': 2,
+      },
+      tags: ['tag2', 'tag4'],
+    }
+] 
+  
+
+  const games = (
+    <div className={styles.wrapper}>
+      <div className={styles.pagination_container}>
+        <Pagination
+          items={items}
+          siblingCount={2}
+        />
+      </div>
+    </div> 
   )
+  return games
 }
