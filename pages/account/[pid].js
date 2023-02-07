2624f6b8116ca2f52baffab375f3006da704394e
justin_k_m <justinm0623@gmail.com>
2023-02-06 19:38:07 -0500

    Update [pid].js
    Delete operation, some UI

--- pages/account/[pid].js
+++ pages/account/[pid].js
@@ -1,5 +1,52 @@
+import styles from '@/scss/account.module.scss'
+
+import { useMutation, useReactiveVar, gql } from '@apollo/client'
+
+import {r_user} from 'models/cache'
+
 export default function Account() {
+
+  const userInfo = useReactiveVar(r_user)
+
+  const DELETE_ACCOUNT = gql`
+    mutation DeleteAccount($email: String!) {
+      deleteAccount(email: $email)
+    }
+  `
+
+  const [deleteAccount] = useMutation(DELETE_ACCOUNT, {
+    variables: {
+      email: userInfo.email
+    }
+  })
+
+  function handleDeleteAccount() {
+    deleteAccount(userInfo.email).then((resp) => {
+      console.log(resp)
+    })
+  }
+
   return (
-    <div>Account</div>
+    <div className={styles.body}>
+      <div className={styles.banner}/>
+      
+      <div className={styles.title_container}><h2>Account Page</h2></div>
+
+      <div className={styles.user_info__container}>
+        <div className={styles.user_info__icon__container}>
+          <div className={styles.user_info__icon__background}></div>
+        </div>
+        <div className={styles.user_info__data__container}>
+          <div className={styles.user_info__data__username}>{userInfo.username}</div>
+          <div className={styles.user_info__data__email}>{userInfo.email}</div>
+          <div>advert</div>
+        </div>
+      </div>
+
+      <button 
+        className={styles.delete_account}
+        onClick={handleDeleteAccount}
+      >Delete Account</button>
+    </div> 
   )
 }
